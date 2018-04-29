187563b0c1e54574caad423d3a2f9c7c02be9a08
XiuYu.Ge <861719405@qq.com>
2018-04-29 22:22:38 +0800

    .

--- quick-user/quick-user-service/src/main/java/cn/xiuyu/core/dao/authentication/GroupRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.xiuyu.core.dao.authentication;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cn.xiuyu.core.model.authentication.GroupModel;
-
-public interface GroupRepository extends JpaRepository<GroupModel, Integer> {
-
-}
--- quick-user/quick-user-service/src/main/java/cn/xiuyu/core/dao/authentication/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.xiuyu.core.dao.authentication;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cn.xiuyu.core.model.authentication.UserModel;
-
-public interface UserRepository extends JpaRepository<UserModel, Integer> {
-
-}
